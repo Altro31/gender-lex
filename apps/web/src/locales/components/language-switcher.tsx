d1aeeb6d028f5781e64d4b02e3d489e653b3effb
Altro <albe020531@outlook.com>
2025-11-02 14:55:40 -0500

    fix

--- apps/web/src/locales/components/language-switcher.tsx
+++ apps/web/src/locales/components/language-switcher.tsx
@@ -1,62 +1,52 @@
-"use client"
+'use client'
 
 import {
 	DropdownMenuItem,
 	DropdownMenuSub,
 	DropdownMenuSubContent,
 	DropdownMenuSubTrigger,
-} from "@/components/ui/dropdown-menu"
-import { setLanguage } from "@/services/registers"
-import { i18n } from "@lingui/core"
-import { t } from "@lingui/core/macro"
-import { Loader2 } from "lucide-react"
-import { useRouter } from "next/navigation"
-import { useTransition } from "react"
-import { toast } from "sonner"
+} from '@/components/ui/dropdown-menu'
+import { setLanguage } from '@/services/registers'
+import { t } from '@lingui/core/macro'
+import { useParams, useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 export default function LanguageSwitcher() {
 	const router = useRouter()
-	const locale = i18n.locale
-	const [loading, startTransition] = useTransition()
+	const { locale } = useParams()
 
-	const changeLanguage = (value: string) => () => {
-		startTransition(async () => {
-			const { serverError } = await setLanguage(value)
-			if (serverError) {
-				console.log(serverError)
-				toast.error("Error")
-				return
-			}
-			const url = new URL(window.location.href)
-			const segments = url.pathname.split("/")
-			segments[1] = value
-			url.pathname = segments.join("/")
-			router.replace(url.toString())
-		})
+	const changeLanguage = (value: string) => async () => {
+		const { serverError } = await setLanguage(value)
+		if (serverError) {
+			console.log(serverError)
+			toast.error('Error')
+			return
+		}
+		const url = new URL(window.location.href)
+		const segments = url.pathname.split('/')
+		segments[1] = value
+		url.pathname = segments.join('/')
+		router.replace(url.toString())
 	}
 
 	return (
 		<>
 			<DropdownMenuSub>
 				<DropdownMenuSubTrigger className="gap-2">
-					{loading ? (
-						<Loader2 className="size-4 animate-spin" />
-					) : (
-						<span className="rounded-sm border px-1">{locale}</span>
-					)}
+					<span className="rounded-sm border px-1">{locale}</span>
 					{t`Language`}
 				</DropdownMenuSubTrigger>
 				<DropdownMenuSubContent>
 					<DropdownMenuItem
-						onClick={changeLanguage("en")}
-						data-active={locale === "en" || loading || undefined}
+						onClick={changeLanguage('en')}
+						data-active={locale === 'en' || undefined}
 						className="data-active:pointer-events-none data-active:opacity-50"
 					>
 						{t`English`}
 					</DropdownMenuItem>
 					<DropdownMenuItem
-						onClick={changeLanguage("es")}
-						data-active={locale === "es" || loading || undefined}
+						onClick={changeLanguage('es')}
+						data-active={locale === 'es' || undefined}
 						className="data-active:pointer-events-none data-active:opacity-50"
 					>
 						{t`Spanish`}
