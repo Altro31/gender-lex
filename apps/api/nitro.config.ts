170dc89355604403987fbf30b2f81bf766b7da63
Altro31 <albe020531@outlook.com>
2025-12-19 07:11:01 -0500

    fix

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "nitro"
+import { defineConfig } from 'nitro'
 
 export default defineConfig({
-    routes: { "/**": "./src/index.ts" },
-    modules: ["workflow/nitro"],
+	routes: { '/**': './src/index.ts' },
+	modules: ['workflow/nitro'],
+	plugins: ['nitro/plugins/start-pg-world.ts'],
 })
