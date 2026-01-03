58b1605a35d5baa4e6f5de28ae2eeb5d6aa42803
Altro31 <albe020531@outlook.com>
2026-01-03 12:13:47 -0500

    fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,51 +1,53 @@
-import analysis from '@/modules/analysis'
-import chatbot from '@/modules/chatbot'
-import model from '@/modules/model'
-import sse from '@/modules/sse'
-import zen from '@/modules/zen'
-import { cors } from '@elysiajs/cors'
-import openapi, { fromTypes } from '@elysiajs/openapi'
-import { auth } from '@repo/auth/api'
-import { Elysia } from 'elysia'
-import z from 'zod'
-import { authPlugin } from './plugins/auth.plugin'
-import { effectPlugin } from './plugins/effect.plugin'
-import { JSONSchema } from 'effect'
-import { start } from 'workflow/api'
-import { testWorkflow } from './workflows/test'
+import analysis from "@/modules/analysis"
+import chatbot from "@/modules/chatbot"
+import model from "@/modules/model"
+import sse from "@/modules/sse"
+import zen from "@/modules/zen"
+import { cors } from "@elysiajs/cors"
+import openapi, { fromTypes } from "@elysiajs/openapi"
+import { auth } from "@repo/auth/api"
+import { Elysia } from "elysia"
+import z from "zod"
+import { authPlugin } from "./plugins/auth.plugin"
+import { effectPlugin } from "./plugins/effect.plugin"
+import { JSONSchema } from "effect"
+import { start } from "workflow/api"
+import { testWorkflow } from "./workflows/test"
+import { defaultModelsAndPresetsPlugin } from "./plugins/defaults-models-and-presets"
 
 export type App = typeof app
 const app = new Elysia()
-	.use(cors())
-	.mount(auth.handler)
-	.use(
-		openapi({
-			references: fromTypes(),
-			documentation: {
-				info: { title: 'Gender Lex API', version: '1.0.0' },
-				externalDocs: {
-					url: '/api/auth/reference',
-					description: 'Auth',
-				},
-			},
-			mapJsonSchema: {
-				effect: JSONSchema.make,
-				zod: (arg: any) =>
-					z.toJSONSchema(arg, { unrepresentable: 'any' }),
-			},
-		}),
-	)
-	.use(authPlugin)
-	.use(effectPlugin)
-	.use(sse)
-	.use(model)
-	.use(analysis)
-	.use(chatbot)
-	.use(zen)
-	.get('/', () => ({ ok: true }))
-	.get('/workflow', async () => {
-		await start(testWorkflow, [])
-		return { message: 'User signup workflow started' }
-	})
+    .use(cors())
+    .mount(auth.handler)
+    .use(
+        openapi({
+            references: fromTypes(),
+            documentation: {
+                info: { title: "Gender Lex API", version: "1.0.0" },
+                externalDocs: {
+                    url: "/api/auth/reference",
+                    description: "Auth",
+                },
+            },
+            mapJsonSchema: {
+                effect: JSONSchema.make,
+                zod: (arg: any) =>
+                    z.toJSONSchema(arg, { unrepresentable: "any" }),
+            },
+        }),
+    )
+    .use(authPlugin)
+    .use(effectPlugin)
+    .use(defaultModelsAndPresetsPlugin)
+    .use(sse)
+    .use(model)
+    .use(analysis)
+    .use(chatbot)
+    .use(zen)
+    .get("/", () => ({ ok: true }))
+    .get("/workflow", async () => {
+        await start(testWorkflow, [])
+        return { message: "User signup workflow started" }
+    })
 
 export default app.compile()
--- apps/api/src/plugins/effect.plugin.ts
+++ apps/api/src/plugins/effect.plugin.ts
@@ -1,32 +1,32 @@
-import { ContextService } from '@/shared/context.service'
-import { Cause, Effect, Exit, Logger, LogLevel, ManagedRuntime } from 'effect'
-import Elysia from 'elysia'
-import type { Prettify } from 'elysia/types'
+import { ContextService } from "@/shared/context.service"
+import { Cause, Effect, Exit, Logger, LogLevel, ManagedRuntime } from "effect"
+import Elysia from "elysia"
+import type { Prettify } from "elysia/types"
 
-export const effectPlugin = new Elysia({ name: 'plugin.auth' })
-	.decorate(
-		'runEffectWithContext',
-		(ctx: any) =>
-			<S, E>(effect: Effect.Effect<S, E, ContextService>) =>
-				ManagedRuntime.make(ContextService.Default(ctx))
-					.runPromiseExit(
-						effect.pipe(Logger.withMinimumLogLevel(LogLevel.Debug)),
-					)
-					.then((res): Prettify<S> => {
-						console.log(
-							'Effect result:',
-							JSON.stringify(res, null, '\t'),
-						)
-						if (Exit.isSuccess(res)) return res.value
-						const cause = res.cause
-						if (Cause.isFailType(cause)) {
-							throw cause.error
-						}
-						return {} as Prettify<S>
-					}),
-	)
-	.derive(ctx => ({
-		runtime: ManagedRuntime.make(ContextService.Default(ctx)),
-		runEffect: ctx.runEffectWithContext(ctx),
-	}))
-	.as('scoped')
+export const effectPlugin = new Elysia({ name: "plugin.effect" })
+    .decorate(
+        "runEffectWithContext",
+        (ctx: any) =>
+            <S, E>(effect: Effect.Effect<S, E, ContextService>) =>
+                ManagedRuntime.make(ContextService.Default(ctx))
+                    .runPromiseExit(
+                        effect.pipe(Logger.withMinimumLogLevel(LogLevel.Debug)),
+                    )
+                    .then((res): Prettify<S> => {
+                        console.log(
+                            "Effect result:",
+                            JSON.stringify(res, null, "\t"),
+                        )
+                        if (Exit.isSuccess(res)) return res.value
+                        const cause = res.cause
+                        if (Cause.isFailType(cause)) {
+                            throw cause.error
+                        }
+                        return {} as Prettify<S>
+                    }),
+    )
+    .derive(ctx => ({
+        runtime: ManagedRuntime.make(ContextService.Default(ctx)),
+        runEffect: ctx.runEffectWithContext(ctx),
+    }))
+    .as("scoped")
