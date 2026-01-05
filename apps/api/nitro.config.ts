8ac7a0bf1213a1830a5c2d9e3fa80de7f913e15a
Altro31 <albe020531@outlook.com>
2026-01-05 16:43:02 -0500

    fix

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from "nitro"
 export default defineConfig({
     routes: { "/**": "./src/index.ts" },
     modules: ["workflow/nitro"],
-    externals: { external: ["@workflow/world-postgres"] },
-
     plugins: ["nitro/plugins/start-pg-world.ts"],
 })
--- apps/api/nitro/plugins/start-pg-world.ts
+++ apps/api/nitro/plugins/start-pg-world.ts
@@ -1,8 +1,6 @@
 import { defineNitroPlugin } from "nitro/~internal/runtime/plugin"
-import * as pgWorld from "@workflow/world-postgres"
 
 export default defineNitroPlugin(async () => {
-    console.log(pgWorld.schema)
     if (process.env.WORKFLOW_TARGET_WORLD === "@workflow/world-postgres") {
         // Dynamic import to avoid edge runtime bundling issues
         const { getWorld } = await import("workflow/runtime")
