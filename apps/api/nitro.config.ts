18babc64db1cbb355338b902cf428e345ee4de76
Altro31 <albe020531@outlook.com>
2026-01-05 16:36:15 -0500

    Update nitro.config.ts

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "nitro"
 export default defineConfig({
     routes: { "/**": "./src/index.ts" },
     modules: ["workflow/nitro"],
-    externals: { traceInclude: ["@workflow/world-postgres"] },
+    externals: { external: ["@workflow/world-postgres"] },
 
     plugins: ["nitro/plugins/start-pg-world.ts"],
 })
