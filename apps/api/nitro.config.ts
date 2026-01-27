93786b3d6b0860950bb519145ed47f0ba9017649
Altro31 <albe020531@outlook.com>
2026-01-27 01:00:24 -0500

    fix

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -4,6 +4,4 @@ export default defineConfig({
     routes: { "/**": "./src/index.ts" },
     modules: ["workflow/nitro"],
     plugins: ["nitro/plugins/start-pg-world.ts"],
-    runtimeConfig: { app: { idleTimeout: 255 } },
-    preset: "bun",
 })
