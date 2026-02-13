bc16f63d7de46ec9962103017ea43407481a0be7
Altro31 <albe020531@outlook.com>
2026-02-13 16:13:25 -0500

    Enhance environment variable definitions with default values in env-server.ts

--- apps/web/src/lib/env/env-server.ts
+++ apps/web/src/lib/env/env-server.ts
@@ -1,32 +1,48 @@
 import { z } from "zod";
 
 const Envs = z.object({
-  PORT: z.coerce.number().int().positive(),
-  API_URL: z.url(),
-  AUTH_GOOGLE_ID: z.string(),
-  NEXT_PUBLIC_AUTH_GOOGLE_ID: z.string(),
-  AUTH_GOOGLE_SECRET: z.string(),
-  AUTH_GITHUB_ID: z.string(),
-  AUTH_GITHUB_SECRET: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
-  BETTER_AUTH_URL: z.url(),
-  UI_URL: z.url(),
-  NEXT_PUBLIC_UI_URL: z.url(),
-  ENCRYPTION_KEY: z.string().length(32),
-  GEMINI_API_KEY: z.string(),
+  PORT: z.coerce.number().int().positive().default(3000),
+  API_URL: z.url().default("http://localhost:4000"),
+  NEXT_PUBLIC_API_URL: z
+    .url()
+    .default("https://gender-lex-back.mayabeque.mdialityc.com"),
+  AUTH_GOOGLE_ID: z.string().default(""),
+  NEXT_PUBLIC_AUTH_GOOGLE_ID: z
+    .string()
+    .default(
+      "241848770025-7ic9ir9j0adjr8hmpb3lbhdob2kurout.apps.googleusercontent.com"
+    ),
+  AUTH_GOOGLE_SECRET: z.string().default(""),
+  AUTH_GITHUB_ID: z.string().default(""),
+  AUTH_GITHUB_SECRET: z.string().default(""),
+  BETTER_AUTH_SECRET: z.string().default(""),
+  BETTER_AUTH_URL: z.url().default("http://localhost:3000"),
+  UI_URL: z.url().default("https://gender-lex-front.mayabeque.mdialityc.com"),
+  NEXT_PUBLIC_UI_URL: z
+    .url()
+    .default("https://gender-lex-front.mayabeque.mdialityc.com"),
+  ENCRYPTION_KEY: z
+    .string()
+    .length(32)
+    .default("00000000000000000000000000000000"),
+  GEMINI_API_KEY: z.string().default(""),
   // WORKFLOW_TARGET_WORLD: z.enum(['@workflow/world-postgres']).optional(),
   // WORKFLOW_POSTGRES_URL: z.string().optional(),
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().default("postgresql://localhost:5432/genderlex"),
   // WORKFLOW_POSTGRES_JOB_PREFIX: z.string().optional(),
   // WORKFLOW_POSTGRES_WORKER_CONCURRENCY: z.coerce
   // .number()
   // .int()
   // .optional(),
-  NEXT_PUBLIC_VAPID_PUBLIC_KEY: z.string(),
-  VAPID_PRIVATE_KEY: z.string(),
-  CHATBOT_PROVIDER_URL: z.url(),
+  NEXT_PUBLIC_VAPID_PUBLIC_KEY: z
+    .string()
+    .default(
+      "BNYDzPXppmQdVfmPJZ-zmKzEoBaj-HXj7I1G3hP_wvCICVNsTarhuGkA5no1w5wLuGxXiQ1XruKSyBRw0lteZWw"
+    ),
+  VAPID_PRIVATE_KEY: z.string().default(""),
+  CHATBOT_PROVIDER_URL: z.url().default("http://localhost:4000"),
   CHATBOT_PROVIDER_API_KEY: z.string().optional(),
-  CHATBOT_MODEL_IDENTIFIER: z.string(),
+  CHATBOT_MODEL_IDENTIFIER: z.string().default("gemini-1.5-flash"),
 });
 // .superRefine(
 // 	(
